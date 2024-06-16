ff3a8ff321018c034d2209097bc14dbf26a5436d
aditya8850 <kalsotraaditya98@gmail.com>
2024-06-16 17:51:27 +0530

    added-indexes-in-db

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,4 +1,5 @@
 import { MongoClient } from "mongodb";
+import ApplicationError from "../error-handler/applicationError.js";
 // import dotenv from "dotenv";
 // dotenv.config();
 let client;
@@ -7,6 +8,8 @@ export const connectToMongoDB=()=>{
     .then(clientInstance=>{
         client= clientInstance;
         console.log("mongodb is connected");
+        createCounter(client.db());
+        createIndexes(client.db());
     })
     .catch(err=>{
         console.log(err);
@@ -14,4 +17,20 @@ export const connectToMongoDB=()=>{
 }
 export const getDB=()=>{
     return client.db();
+}
+const createCounter = async(db)=>{
+    const existingCounter = await db.collection("counters").findOne({_id:'cartItemId'})
+    if(!existingCounter){
+        await db.collection("counters").insertOne({_id:'cartItemId',value:0})
+    }
+}
+const createIndexes = async(db) => {
+   try {
+     await db.collection("products").createIndex({ price:1}); //single indexes
+     await db.collection('products').createIndex({name:1,category:-1}) //compound indexes
+     await db.collection('products').createIndex({desc:"text"}) //text based description
+     console.log("indexes created in db");
+   } catch (error) {
+        throw new ApplicationError("index creation failed",400)
+   }
 }
--- src/features/cartItems/cartItems.repository.js
+++ src/features/cartItems/cartItems.repository.js
@@ -1,5 +1,5 @@
 import { getDB } from "../../config/mongodb.js"
-import { ObjectId } from "mongodb";
+import { ObjectId,  } from "mongodb";
 import ApplicationError from "../../error-handler/applicationError.js";
 
 class CartItemsRepository {
@@ -10,14 +10,16 @@ class CartItemsRepository {
         try {
             const db = getDB();
             const collection = db.collection(this.collection);
+            const id = await this.getNextCounter(db)
             //insertion
             //we need to check if theres already a cartitem,then increment either insert
             //update takes three parameters first is to filter , 2nd is the data to be updated and third one is options
             await collection.updateOne({ productId: new ObjectId(productId), userId: new ObjectId(userId) },
-            {$inc:{
-                quantity:quantity
-            }},
-            {upsert:true})
+                {
+                    $setOnInsert:{_id:id},
+                    $inc: {quantity: quantity}
+                },
+                { upsert: true })
         } catch (error) {
             console.error(error);
             throw new ApplicationError("something wrong with db", 500)
@@ -34,17 +36,26 @@ class CartItemsRepository {
             throw new ApplicationError("something wrong with db", 500)
         }
     };
-    async delete(userId,cartItemId) {
+    async delete(userId, cartItemId) {
         try {
             const db = getDB();
             const collection = db.collection(this.collection);
-            const result= await collection.deleteOne({_id: new ObjectId(cartItemId),userId:new ObjectId(userId)});
-            return result.deletedCount>0;
-    } catch(error) {
-        console.error(error);
-        throw new ApplicationError("something wrong with db", 500)
+            const result = await collection.deleteOne({ _id: new ObjectId(cartItemId), userId: new ObjectId(userId) });
+            return result.deletedCount > 0;
+        } catch (error) {
+            console.error(error);
+            throw new ApplicationError("something wrong with db", 500)
+        }
     }
-}
+    async getNextCounter(db){
+        const counter = await db.collection("counters").findOneAndUpdate(
+            {_id: "cartItemId",},
+            {$inc:{value:1}},
+            {returnDocument:'after'}
+        )
+        console.log(counter);
+        return counter.value.value;
+    }
 
 }
 export default CartItemsRepository
